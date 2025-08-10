38b644a861fc41615a436693067cb44010692486
agungalby30-lab <agungalby30@gmail.com>
2025-08-10 15:34:27 +0700

    Create blogtest1.js

---
+++ blogtest1.js
@@ -0,0 +1,26 @@
+var dataUrl = [
+
+  'https://videoplaytera12.blogspot.com/2025/06/play-video-20.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-19.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-18.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-17.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-16.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-15.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-14.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-6.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-4.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-13.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-7.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-5.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-3.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-12.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-8.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-2.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-11.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-9.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-1.html',
+'https://videoplaytera12.blogspot.com/2025/06/play-video-10.html'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
